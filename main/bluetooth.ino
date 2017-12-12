cd795aa04011b1e0884a4c96127e07c782afbde8
6a16ec <semaev2014@yandex.ru>
2017-12-12 23:58:49 +0300

    test

--- main/bluetooth.ino
+++ main/bluetooth.ino
@@ -10,15 +10,11 @@ void bluetoothMonitoring()
 { 
 
 if (Serial.available()) { // если пришли данные по Bluetooth 
-	data[count_data-1] = Serial.read(); // записываем принятые данные, в последний элемент массива 
-	if (data[count_data-1] == 32 && data[count_data-2] == 48) pinMode(12, OUTPUT); // Перед началом загрузки sketch, еще до RESET, поступает (DTR сигнал) (48 и 32) три раза. ASCII Code (48 = Zero / 0) - (32 = Space / пробел). проверяем если пришёл (DTR сигнал), делаем RESET. ( на Pro Mini, PIN-A3 соединён через Резистор 220 Ом с PIN-RESET) 
-	
-	for(int i = 0; i < count_data-1; i++)
-		data[i] = data[i+1]
-}
-else
-{
-
+  data[count_data-1] = Serial.read(); // записываем принятые данные, в последний элемент массива 
+  if (data[count_data-1] == 32 && data[count_data-2] == 48) pinMode(12, OUTPUT); // Перед началом загрузки sketch, еще до RESET, поступает (DTR сигнал) (48 и 32) три раза. ASCII Code (48 = Zero / 0) - (32 = Space / пробел). проверяем если пришёл (DTR сигнал), делаем RESET. ( на Pro Mini, PIN-A3 соединён через Резистор 220 Ом с PIN-RESET) 
+  
+  for(int i = 0; i < count_data-1; i++)
+    data[i] = data[i+1];
 }
 /* 
 if(Serial.available()) 
@@ -29,3 +25,4 @@ if(character == '\n') Serial.print("LINE! ok");
 } 
 */ 
 }
+

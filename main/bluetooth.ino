2aeb7357d0857445b9df3389ea873fd2e8b5091b
6a16ec <semaev2014@yandex.ru>
2017-12-12 23:46:29 +0300

    new bt

--- main/bluetooth.ino
+++ main/bluetooth.ino
@@ -1,4 +1,5 @@
-byte ResetDTR[2]; 
+#define count_data 20
+byte data[count_data] = {0};
 
 void bluetoothSetup() 
 { 
@@ -9,10 +10,16 @@ void bluetoothMonitoring()
 { 
 
 if (Serial.available()) { // если пришли данные по Bluetooth 
-ResetDTR[0] = Serial.read(); // записываем принятые данные, в первый элемент массива 
-if (ResetDTR[0] == 32 && ResetDTR[1] == 48) pinMode(12, OUTPUT); // Перед началом загрузки sketch, еще до RESET, поступает (DTR сигнал) (48 и 32) три раза. ASCII Code (48 = Zero / 0) - (32 = Space / пробел). проверяем если пришёл (DTR сигнал), делаем RESET. ( на Pro Mini, PIN-A3 соединён через Резистор 220 Ом с PIN-RESET) 
-ResetDTR[1] = ResetDTR[0];// записываем значение из первого элемент массива - во второй элемент массива 
-} 
+	data[count_data-1] = Serial.read(); // записываем принятые данные, в последний элемент массива 
+	if (data[count_data-1] == 32 && data[count_data-2] == 48) pinMode(12, OUTPUT); // Перед началом загрузки sketch, еще до RESET, поступает (DTR сигнал) (48 и 32) три раза. ASCII Code (48 = Zero / 0) - (32 = Space / пробел). проверяем если пришёл (DTR сигнал), делаем RESET. ( на Pro Mini, PIN-A3 соединён через Резистор 220 Ом с PIN-RESET) 
+	
+	for(int i = 0; i < count_data-1; i++)
+		data[i] = data[i+1]
+}
+else
+{
+
+}
 /* 
 if(Serial.available()) 
 { 

45cb117b5573b4ffaf6853dbdab245c4a042d498
6a16ec <semaev2014@yandex.ru>
2018-01-04 20:53:07 +0300

    added start-stop system

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,7 @@
 bool  sensors[8]; /* (!) requires revision (!) */
 int  sensors_int[8]; /* (!) requires revision (!) */
 
-
+bool robot_work = true;
 
 /* ### METHODS ###*/
 
@@ -12,8 +12,6 @@ void setup()
   beepSetup();
 
   motorsSetup();
-  motorsON();
-  motors(0, 0);
   motorsForward();
 
   sensorsSetup();
@@ -21,23 +19,22 @@ void setup()
   old_sensors_values();
   EEPROM_setup();
   bluetoothSetup();
-  pinMode (2, OUTPUT);
-  // pish();
-
-
 }
 
 void loop()
 {
   readLine();
-  beep_check();
-  //chek_sensors();
   led_sensors_indication();
   algorithm2();
-  // check_turns();
-  //chek_perek();
-  //blue_debug();
+  beep_check();
   bluetoothMonitoring();
+
+  if(!robot_work)
+  {
+    motors(0, 0);
+
+    while(!robot_work) bluetoothMonitoring();
+  }
 
 }
 
